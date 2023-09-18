8fd39f201e43bef9e5bcfde96c3825370d9a7c2b
dr33k <dr33k@users.noreply.github.com>
2023-09-18 21:26:36 +0100

    Coloured X and O boxes

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -3,7 +3,9 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-square',
   template: `
-  <button mat-flat-button color="primary" [disabled]=disabled>{{value}}</button>
+  <button mat-flat-button *ngIf="!value" color="primary" [disabled]=disabled>{{value}}</button>
+  <button mat-flat-button *ngIf="value == 'X'" color="accent" [disabled]=disabled>{{value}}</button>
+  <button mat-flat-button *ngIf="value == 'O'" color="warn" [disabled]=disabled>{{value}}</button>
   `,
   styles: []
 })
