b906fffdcab5441e2dbff48a288da82570594174
dr33k <dr33k@users.noreply.github.com>
2023-09-19 09:55:26 +0100

    Added PWA feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { initializeApp } from "firebase/app";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tic-tac-toe';
+
+  firebaseConfig = {
+    apiKey: "AIzaSyAO-quiG-RMvt4w3RNnmXolG_d-sMUBTyQ",
+    authDomain: "fir-be-bdffc.firebaseapp.com",
+    projectId: "fir-be-bdffc",
+    storageBucket: "fir-be-bdffc.appspot.com",
+    messagingSenderId: "98725025062",
+    appId: "1:98725025062:web:82bcedc6bc9fed99881de7"
+  };
+
+  app = initializeApp(this.firebaseConfig);
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,6 +7,7 @@ import { SquareComponent } from './square/square.component';
 import { BoardComponent } from './board/board.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,13 @@ import { MatButtonModule } from '@angular/material/button';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
     ],
   providers: [],
   bootstrap: [AppComponent]
